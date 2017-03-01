de18dfac52509b340026ba0d5424da2aa2c1d83f
KoAi <adrien.le.clair@gmail.com>
2017-03-01 14:05:57 +0100

    Ajout package telnet

--- cookbooks/myfirst_cookbook_07/recipes/default.rb
+++ cookbooks/myfirst_cookbook_07/recipes/default.rb
@@ -1,8 +1,9 @@
 #
-# Cookbook:: myfirst_cookbook
+# Cookbook:: myfirst_cookbook_07
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 package 'vim'
 package 'lynx'
+package 'telnet'
