d3dd5e7a7d789e15dca07cf63d430c60defb60f7
KoAi <adrien.le.clair@gmail.com>
2017-03-01 14:04:30 +0100

    Update metadata.rb

--- cookbooks/myfirst_cookbook_07/metadata.rb
+++ cookbooks/myfirst_cookbook_07/metadata.rb
@@ -1,9 +1,9 @@
-name 'myfirst_cookbook'
+name 'myfirst_cookbook_07'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
-description 'Installs/Configures myfirst_cookbook'
-long_description 'Installs/Configures myfirst_cookbook'
+description 'Installs/Configures myfirst_cookbook_07'
+long_description 'Installs/Configures myfirst_cookbook_07'
 version '0.2.0'
 
 issues_url 'https://github.com/oswaldderiemaecker/chef-repo-training/myfirst_cookbook/issues' if respond_to?(:issues_url)
