1fc7476e3a45ec9c2f05e16e9db6843cba54f574
Workstation3 <kervoaz@gmail.com>
2017-03-02 17:15:49 +0100

    add java

--- cookbooks/myfirst_cookbook_03/test/smoke/default/default_test.rb
+++ cookbooks/myfirst_cookbook_03/test/smoke/default/default_test.rb
@@ -3,20 +3,24 @@
 # Inspec test for recipe myfirst_cookbook::default
 
 # The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
 unless os.windows?
   describe user('root') do
     it { should exist }
-    skip 'This is an example test, replace with your own test.'
   end
 end
 
 describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+  it { should be_listening }
 end
 
-describe command('vim --help') do
-its('stdout') {should match (/VIM -Vi IMproved/)}
+describe package('unzip') do
+  it { should be_installed }
+end
+
+describe command('unzip') do
+  its('stdout') { should match(/UnZip/) }
+end
+
+describe command('java -version') do
+  its('stdout') { should match(/java/) }
 end
