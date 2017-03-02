ca5e0e3777a09bc1b9ce476f97bc3adb75b44d73
KoAi <adrien.le.clair@gmail.com>
2017-03-02 10:13:47 +0100

    promote myfirst_cookbook07@0.2.0 to staging07

--- cookbooks/myfirst_cookbook07/metadata.rb
+++ cookbooks/myfirst_cookbook07/metadata.rb
@@ -4,16 +4,16 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures myfirst_cookbook07'
 long_description 'Installs/Configures myfirst_cookbook07'
-version '0.1.0'
+version '0.2.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/myfirst_cookbook07/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/KoAi/chef-repo-training/myfirst_cookbook07/issues' if respond_to?(:issues_url)
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/myfirst_cookbook07' if respond_to?(:source_url)
+source_url 'https://github.com/KoAi/chef-repo-training/myfirst_cookbook07' if respond_to?(:source_url)
