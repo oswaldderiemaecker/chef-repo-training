cfdf8aaf472a3cac66cbbd993e64611c1e78473c
Workstation3 <kervoaz@gmail.com>
2017-03-02 17:06:46 +0100

    promote myfirst_cookbook_03@0.6.1 to development_03

--- cookbooks/myfirst_cookbook_03/metadata.rb
+++ cookbooks/myfirst_cookbook_03/metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures myfirst_cookbook'
 long_description 'Installs/Configures myfirst_cookbook'
-version '0.6.0'
+version '0.6.1'
 
 issues_url 'https://github.com/oswaldderiemaecker/chef-repo-training/myfirst_cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/oswaldderiemaecker/chef-repo-training/myfirst_cookbook' if respond_to?(:source_url)
+
+depends 'chef_nginx', '~> 5.0.7'
