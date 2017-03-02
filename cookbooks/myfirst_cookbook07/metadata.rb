b3fc20106be90dbb10b1bf2f35ca3089e35dd205
KoAi <adrien.le.clair@gmail.com>
2017-03-02 17:27:55 +0100

    promote myfirst_cookbook07@0.5.0 to development07

--- cookbooks/myfirst_cookbook07/metadata.rb
+++ cookbooks/myfirst_cookbook07/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures myfirst_cookbook07'
 long_description 'Installs/Configures myfirst_cookbook07'
-version '1.0.0'
+version '0.5.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
@@ -19,3 +19,4 @@ issues_url 'https://github.com/KoAi/chef-repo-training/myfirst_cookbook07/issues
 source_url 'https://github.com/KoAi/chef-repo-training/myfirst_cookbook07' if respond_to?(:source_url)
 
 depends 'chef_nginx', '~> 5.0.7'
+depends 'java', '~> 1.47.0'
