a20055db750c54fa583166b9d6cd27d8d431ab98
KoAi <adrien.le.clair@gmail.com>
2017-03-02 15:18:25 +0100

    kitchen

--- cookbooks/myfirst_cookbook07/test/smoke/default/default_test.rb
+++ cookbooks/myfirst_cookbook07/test/smoke/default/default_test.rb
@@ -8,11 +8,17 @@
 unless os.windows?
   describe user('root') do
     it { should exist }
-    skip 'This is an example test, replace with your own test.'
   end
 end
 
 describe port(80) do
   it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+end
+
+describe package('unzip') do
+  it { should be_installed }
+end
+
+describe command('unzip') do
+   its('stdout') { should match (/UnZip 6/) }
 end
