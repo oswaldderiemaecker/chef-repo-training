cc24b7ab9de759dd5b6218307c4948adfdb5fb10
nicodd <nicodd@mail.com>
2017-03-03 09:59:54 +0100

    branch

--- cookbooks/myfirst_cookbook_04/metadata.rb
+++ cookbooks/myfirst_cookbook_04/metadata.rb
@@ -4,11 +4,10 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures myfirst_cookbook'
 long_description 'Installs/Configures myfirst_cookbook'
-version '0.6.0'
+version '0.7.0'
 
 issues_url 'https://github.com/oswaldderiemaecker/chef-repo-training/myfirst_cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/oswaldderiemaecker/chef-repo-training/myfirst_cookbook' if respond_to?(:source_url)
 
 depends 'chef_nginx', '~> 5.1.0'
 depends 'java', '~> 1.47.0'
-
